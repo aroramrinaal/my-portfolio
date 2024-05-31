70b4b488010cf975d19c957975f34b79cac8e653
aroramrinaal <apply.mrinaal@gmail.com>
2024-05-31 14:23:12 -0700

    basic navbar using tailwind.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
+import Navbar from "@/components/Navbar";
 import React from "react";
 
 const Index = () => {
   return(
-    <div className="text-center text-green-500" >
-      <h1>Hello World</h1>
+    <div>
+      <Navbar />
     </div>
   );
 };
