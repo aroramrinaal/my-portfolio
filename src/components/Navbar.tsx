d0d9dfc13bd93cd29ef64990e076d12cedece646
aroramrinaal <apply.mrinaal@gmail.com>
2024-06-01 11:53:38 -0700

    feat: update Navbar link to projects page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = () => {
                             <div className="ml-10 flex items-baseline space-x-4">
                                 <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Home</a>
                                 <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About</a>
-                                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Projects</a>
+                                <a href="/projects" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Projects</a>
                                 <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Blog</a>
                             </div>
                         </div>
