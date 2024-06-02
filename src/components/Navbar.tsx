4265e9535ae477dd1f9303304fbd6658c7fd800e
aroramrinaal <apply.mrinaal@gmail.com>
2024-06-02 13:17:36 -0700

    changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,7 +21,7 @@ const Navbar: React.FC = () => {
                     <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
                         <li>
                             <Link href="/projects">
-                                <a className="block py-2 px-3 md:p-0 text-lg text-amethyst rounded md:bg-transparent dark:text-amethyst">Projects</a>
+                                <a className="block py-2 px-3 md:p-0 text-lg text-black rounded md:bg-transparent dark:text-amethyst">Projects</a>
                             </Link>
                         </li>
                         <li>
