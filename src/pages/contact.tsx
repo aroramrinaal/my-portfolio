f4b916e1fedd63b53827e6b0c78106e35325cbfc
aroramrinaal <apply.mrinaal@gmail.com>
2024-06-02 13:52:41 -0700

    refactor: Update contact page message for better readability

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -9,7 +9,7 @@ const Contact = () => {
             <main className="flex-grow bg-linen dark:bg-gray-900">
                 <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
                     <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-amethyst dark:text-white">Contact Me</h2>
-                    <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Have a project in mind or just want to say hi? Drop me a message! Have any opportunities you would like to discuss? I'd love to hear from you!</p>
+                    <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Have a project in mind or just want to say hi? Drop me a message! Have any opportunities you would like to discuss? I would love to hear from you!</p>
                     <form action="#" className="space-y-8">
                         <div>
                             <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
