07a31bd5db7767847e88e8f9bd1c90dbaeb851df
aroramrinaal <apply.mrinaal@gmail.com>
2024-06-02 11:55:45 -0700

    refactor: Update contact page heading color

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -7,7 +7,7 @@ const Contact = () => {
             <Navbar />
             <section className="bg-linen dark:bg-gray-900">
                 <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-                    <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Me</h2>
+                    <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-amethyst dark:text-white">Contact Me</h2>
                     <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Have a project in mind or just want to say hi? Drop me a message! Have any opportunities you would like to discuss? Id love to hear from you!</p>
                     <form action="#" className="space-y-8">
                         <div>
