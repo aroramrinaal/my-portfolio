19ae7e6409c68f4931c220b7ac87e1d912fd4a6d
aroramrinaal <apply.mrinaal@gmail.com>
2024-06-02 08:56:01 -0700

    refactor: Remove unused components and update homepage content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
-import Navbar from '@/components/Navbar';
-import Footer from '@/components/Footer';
 
 const Index: React.FC = () => {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Navbar />
-      <main className="flex-grow container mx-auto mt-8">
-        <h1 className="text-4xl font-bold">Welcome to My Portfolio</h1>
-        <p className="mt-4">This is the home page of my portfolio website.</p>
-      </main>
-      <Footer />
+    <div>
+      <h1>Hello, World!</h1>
     </div>
   );
 };
