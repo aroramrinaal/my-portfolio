3aff4b554d573ae1be7013116b5c0cfd49dd8c2a
aroramrinaal <apply.mrinaal@gmail.com>
2024-06-02 11:33:01 -0700

    refactor: Update contact page styles and content

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import Contact from '@/pages/contact';
 
 const Navbar: React.FC = () => {
     return (
-        <nav className="border-gray-200 bg-darkPurple dark:bg-gray-800 dark:border-gray-700">
+        <nav className="border-gray-200 bg-linen dark:bg-gray-800 dark:border-gray-700">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <Link href="/">
                     <a className="flex items-center space-x-3 rtl:space-x-reverse">
@@ -22,22 +22,22 @@ const Navbar: React.FC = () => {
                     <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
                         <li>
                             <Link href="/projects">
-                                <a className="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent" aria-current="page">Projects</a>
+                                <a className="block py-2 px-3 md:p-0 text-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent" aria-current="page">Projects</a>
                             </Link>
                         </li>
                         <li>
                             <Link href="/blog">
-                                <a className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Blog</a>
+                                <a className="block py-2 px-3 md:p-0 text-blue-700 rounded md:hover:bg-transparent md:border-0 md:text-blue-700 dark:text-white md:dark:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Blog</a>
                             </Link>
                         </li>
                         <li>
                             <Link href="/resume">
-                                <a className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Resume</a>
+                                <a className="block py-2 px-3 md:p-0 text-blue-700 rounded md:hover:bg-transparent md:border-0 md:text-blue-700 dark:text-white md:dark:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Resume</a>
                             </Link>
                         </li>
                         <li>
                             <Link href="/contact">
-                                <a className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+                                <a className="block py-2 px-3 md:p-0 text-blue-700 rounded md:hover:bg-transparent md:border-0 md:text-blue-700 dark:text-white md:dark:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
                             </Link>
                         </li>
                     </ul>
