a9580e43aecc9733db4b4c19f98cadc36065ee15
aroramrinaal <apply.mrinaal@gmail.com>
2024-06-02 13:01:56 -0700

    refactor: Update Navbar logo source to use local file

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar: React.FC = () => {
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <Link href="/">
                     <a className="flex items-center space-x-3 rtl:space-x-reverse">
-                        <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="MyLogo" />
+                        <img src="public/mylogo.svg" className="h-8" alt="MyLogo" />
                         <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Mrinaal Arora</span>
                     </a>
                 </Link>
