75f9a9946d4f16284ee66968b97532623002c669
aroramrinaal <apply.mrinaal@gmail.com>
2024-06-04 12:52:30 -0700

    ft: projects page.

---
+++ src/pages/projects.tsx
@@ -0,0 +1,24 @@
+
+import React from 'react';
+import Navbar from '@/components/Navbar';
+import ProjectCard from '@/components/ProjectCard';
+
+const Projects: React.FC = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-grow bg-linen dark:bg-gray-900 p-4 md:p-6">
+        <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          <ProjectCard
+            title="Project 1"
+            description="A brief description of the first project."
+            imageUrl="/images/project1.jpg"
+            projectUrl="#"
+          />
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default Projects;
