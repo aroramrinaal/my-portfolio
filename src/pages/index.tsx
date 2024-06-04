12b5928a401f40a2de1e5374810a791c9d0c880c
aroramrinaal <apply.mrinaal@gmail.com>
2024-06-04 12:10:39 -0700

    changes.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ const Home: React.FC = () => {
     <div className="flex flex-col min-h-screen">
       <Navbar />
       <div className="flex flex-grow">
-        <div className="w-2/5 bg-cover bg-center" style={{ backgroundImage: "url('/images/profile.jpeg')", borderRadius: "50%" }}></div>
+        <div className="w-2/5 bg-cover bg-center" style={{ backgroundImage: "url('/images/profile.jpeg')", borderRadius: "5%" }}></div>
         <div className="w-3/5 flex flex-col justify-center items-center bg-linen">
           <div className="text-center">
             <h1 className="text-6xl font-bold text-amethyst">I&apos;m Mrinaal Arora</h1>
