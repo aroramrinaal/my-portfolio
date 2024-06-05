5027d736ad078b502b52b3a7a3634f8c4d134fd1
aroramrinaal <apply.mrinaal@gmail.com>
2024-06-04 23:41:43 -0700

    refactor: Update home page profile picture border color and contact button styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ const Home: React.FC = () => {
       <div className="flex flex-grow items-center justify-center">
         <div className="w-2/5 flex items-center justify-center">
           <div className="relative">
-            <div className="w-96 h-96 rounded-full overflow-hidden border-4 border-black shadow-lg">
+            <div className="w-96 h-96 rounded-full overflow-hidden border-4 border-linen shadow-lg">
               <img 
                 src="/images/profile.jpeg" 
                 alt="Profile Picture" 
@@ -32,7 +32,7 @@ const Home: React.FC = () => {
                 </a>
               </Link>
               <Link href="/contact">
-                <a className="px-6 py-3 text-lg font-medium text-white bg-amethyst border rounded-md hover:bg-gray-100">
+                <a className="px-6 py-3 text-lg font-medium text-black bg-white border border-black rounded-md hover:bg-gray-100">
                   Contact Me
                 </a>
               </Link>
