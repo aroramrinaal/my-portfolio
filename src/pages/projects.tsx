698fda95b1e9057dffdbf141c1a9b5558cdb6bd9
aroramrinaal <apply.mrinaal@gmail.com>
2024-06-04 22:53:12 -0700

    refactor: Update ProjectCard component to include additional projects

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import Navbar from '@/components/Navbar';
 import ProjectCard from '@/components/ProjectCard';
@@ -14,6 +13,36 @@ const Projects: React.FC = () => {
             description="A brief description of the first project."
             imageUrl="/images/project1.jpeg"
             projectUrl="https://github.com/aroramrinaal/spotistats/"
+          />
+          <ProjectCard
+            title="Project 2"
+            description="A brief description of the second project."
+            imageUrl="/images/project2.jpeg"
+            projectUrl="#"
+          />
+          <ProjectCard
+            title="Project 3"
+            description="A brief description of the third project."
+            imageUrl="/images/project3.jpeg"
+            projectUrl="#"
+          />
+          <ProjectCard
+            title="Project 4"
+            description="A brief description of the fourth project."
+            imageUrl="/images/project4.jpeg"
+            projectUrl="#"
+          />
+          <ProjectCard
+            title="Project 5"
+            description="A brief description of the fifth project."
+            imageUrl="/images/project5.jpeg"
+            projectUrl="#"
+          />
+          <ProjectCard
+            title="Project 6"
+            description="A brief description of the sixth project."
+            imageUrl="/images/project6.jpeg"
+            projectUrl="#"
           />
         </section>
       </main>
