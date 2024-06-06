fa14fca62c64b109f2736ac3bfef5ba35d02d352
aroramrinaal <apply.mrinaal@gmail.com>
2024-06-06 00:12:06 -0700

    refactor: Update blog page layout and styling

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Link from 'next/link';
 import Navbar from '@/components/Navbar';
 
+
 interface BlogPost {
   id: string;
   title: string;
@@ -18,20 +19,22 @@ const Blog: React.FC = () => {
   return (
     <div className="flex flex-col min-h-screen bg-linen">
       <Navbar />
-      <div className="flex-grow p-8">
-        <h1 className="text-4xl font-bold mb-8">Read My Blog</h1>
-        <ul>
-          {blogPosts.map((post) => (
-            <li key={post.id} className="mb-8">
-              <Link href={`/blog/${post.id}`}>
-                <a className="text-2xl font-semibold text-black hover:underline">
-                  {post.title}
-                </a>
-              </Link>
-              <p className="text-gray-700">{post.description}</p>
-            </li>
-          ))}
-        </ul>
+      <div className="flex flex-grow items-center justify-center">
+        <div className="p-8 max-w-2xl w-full">
+          <h1 className="text-4xl font-bold mb-8 text-center">Read My Blog</h1>
+          <ul>
+            {blogPosts.map((post) => (
+              <li key={post.id} className="mb-8">
+                <Link href={`/blog/${post.id}`}>
+                  <a className="text-2xl font-semibold text-black hover:underline">
+                    {post.title}
+                  </a>
+                </Link>
+                <p className="text-gray-700">{post.description}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
