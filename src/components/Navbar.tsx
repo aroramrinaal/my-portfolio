adaad45aa2a4b7a9e95ef31adbb2133b61447385
aroramrinaal <apply.mrinaal@gmail.com>
2024-06-07 08:06:37 -0700

    refactor: Add Home link to Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -20,6 +20,11 @@ const Navbar = () => {
                 <div className="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
                     <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
                         <li>
+                            <Link href="/">
+                                <a className="block py-2 px-3 md:p-0 text-lg text-darkPurple rounded md:hover:bg-transparent md:border-0 dark:text-amethyst">Home</a>
+                            </Link>
+                        </li>
+                        <li>
                             <Link href="/about">
                                 <a className="block py-2 px-3 md:p-0 text-lg text-darkPurple rounded md:hover:bg-transparent md:border-0 dark:text-amethyst">About</a>
                             </Link>
