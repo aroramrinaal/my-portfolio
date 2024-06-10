32c47550280e427a32ddf9d5ddff30a8e0b7f18b
aroramrinaal <apply.mrinaal@gmail.com>
2024-06-10 07:58:23 -0700

    refactor: Update contact form labels and input fields

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -11,12 +11,12 @@ const Contact = () => {
                     <p className="mb-8 lg:mb-16 font-light text-center text-gray-500  sm:text-xl">Have a project in mind or just want to say hi? Drop me a message! Have any opportunities you would like to discuss? I&apos;d love to hear from you!</p>
                     <form action="#" className="space-y-8">
                         <div>
-                            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 ">Your email</label>
-                            <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5" placeholder="name@email.com" required />
+                            <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-900">Name</label>
+                            <input type="text" id="subject" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500" placeholder="John Doe" required />
                         </div>
                         <div>
-                            <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-900">Subject</label>
-                            <input type="text" id="subject" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500" placeholder="What you want to talk about?" required />
+                            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 ">Your email</label>
+                            <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5" placeholder="name@email.com" required />
                         </div>
                         <div className="sm:col-span-2">
                             <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900">Your message</label>
