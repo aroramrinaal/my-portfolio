cc80a994320c002e65ad71539692b922d3a83634
aroramrinaal <apply.mrinaal@gmail.com>
2024-08-06 03:32:38 -0700

    refactor: Add Google Analytics to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,18 +3,36 @@ import Head from 'next/head';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Analytics } from '@vercel/analytics/react';
 import '@/styles/globals.css';
+import Script from 'next/script';
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Mrinaal Arora</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="Mrinaal Arora" />
       </Head>
       <Component {...pageProps} />
       <SpeedInsights />
       <Analytics />
+      {/* Google Analytics */}
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-W7GVY05PCJ"
+        strategy="afterInteractive"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+      >
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-W7GVY05PCJ');
+        `}
+      </Script>
     </>
   );
 }
